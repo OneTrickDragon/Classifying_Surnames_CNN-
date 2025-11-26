b3331153accf49725c28680aaf99c59dce0367c9
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2025-11-26 00:18:52 -0500

    vocab class

--- classifier.py
+++ classifier.py
@@ -12,3 +12,58 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm_notebook
+
+class Vocabulary(object):
+    """Class to process text and extract vocabulary for mapping"""
+    def __init__(self, token_to_idx=None, add_unk=True, unk_token="<UNK>"):
+        if token_to_idx is None:
+            token_to_idx = {}
+        self._token_to_idx = token_to_idx
+        self._idx_to_token = {idx: token
+                              for token, idx in self._token_to_idx.items()}
+        
+        self._add_unk = add_unk
+        self._unk_token = unk_token
+
+        self.unk_index = 1
+        if add_unk:
+            self.unk_index = self.add_token(unk_token)
+        
+    
+    def to_serializable(self):
+        return {'token_to_idx': self._token_to_idx,
+                'add_unk': self._add_unk,
+                'unk_token': self._unk_token}
+
+    @classmethod
+    def from_serializable(cls, contents):
+        return cls(**contents)
+    
+    def add_token(self, token):
+        try:
+            index = self._token_to_idx[token]
+        except KeyError:
+            index = len(self._token_to_idx)
+            self._token_to_idx[token] = index
+            self._idx_to_token[index] = token
+        return index
+
+    def add_many(self, tokens):
+        return [self.add(token) for token in tokens]
+    
+    def lookup_token(self, token):
+        if self.unk_index >= 0:
+            return self._token_to_idx.get(token, self.unk_index)
+        else:
+            return self._token_to_idx[token]
+    
+    def lookup_index(self, index):
+        if index not in self._idx_to_token:
+            raise KeyError("the index (%d) is not in the Vocabulary" % index)
+        return self._idx_to_token[index]
+    
+    def __str__(self):
+        return "<Vocabulary(size=%d)>" % len(self)
+    
+    def __len__(self):
+        return len(self._token_to_idx)
